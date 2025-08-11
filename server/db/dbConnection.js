387471df6d5399b7157b3eb1250c7f3758749053
ashank kumar kushwaha <ash.22kk@gmail.com>
2025-08-11 23:14:12 +0530

    dockerizing

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 // Replace with your MongoDB URI
-const uri =`mongodb+srv://${process.env.MONGO_DB_NAME}:${process.env.MONGO_DB_PASS}@data.2leny.mongodb.net/?retryWrites=true&w=majority&appName=data`;
-
+// const uri =`mongodb+srv://${process.env.MONGO_DB_NAME}:${process.env.MONGO_DB_PASS}@data.2leny.mongodb.net/?retryWrites=true&w=majority&appName=data`;
+const uri = 'mongodb://mongo:27017/app';
 let db = null;
 
 async function connectMongo() {
